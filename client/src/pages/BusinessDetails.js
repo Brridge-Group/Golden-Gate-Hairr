b8ceca6f3934ed00d2c48a02abc6206129b0a507
CodeMeKathy * <Kathy@KathyLambert.me>
2021-08-25 20:10:36 -0400

    Feature (<BusinessDetails/>): Add initial customer business details placeholder
    - Add temporary routing link to <BusinessDetails/> in App.js

---
+++ client/src/pages/BusinessDetails.js
@@ -0,0 +1,15 @@
+import ContentHeader from '../components/ContentHeader'
+const BusinessDetails = () => {
+  return (
+    <>
+      <section className='content-wrapper'>
+        <header>
+          <ContentHeader title='Business Details Page' />
+          {/* <h1>Business Details Page</h1> */}
+        </header>
+      </section>
+    </>
+  )
+}
+
+export default BusinessDetails
