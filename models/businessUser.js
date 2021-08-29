52068560b5fe008bbd64c6b0b540c79a86e570f8
CodeMeKathy * <Kathy@KathyLambert.me>
2021-08-28 20:38:31 -0400

    Feature (Business User Model): Update schema with business details

--- models/businessUser.js
+++ models/businessUser.js
@@ -5,7 +5,11 @@ const businessUserSchema = new mongoose.Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  passwordConfirm: { type: String, required: true }
+  passwordConfirm: { type: String, required: true },
+  businessName: { type: String },
+  businessDescription: { type: String },
+  businessAddress: { type: String },
+  businessRating: { type: String }
 })
 
 module.exports = mongoose.model('BusinessUser', businessUserSchema)
