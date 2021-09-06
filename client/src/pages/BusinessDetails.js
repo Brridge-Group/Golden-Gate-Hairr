5d97e6ca01bf934908c5fe0edfb03fe687ef5b11
CodeMeKathy * <Kathy@KathyLambert.me>
2021-09-06 17:15:33 -0400

    Style (<BusinessDetails /> ): Add mobile and desktop styling

--- client/src/pages/BusinessDetails.js
+++ client/src/pages/BusinessDetails.js
@@ -35,28 +35,44 @@ const BusinessDetails = () => {
         </header>
         <div className='card'>
           <div className='card-body'>
-            <ul className='products-list product-list-in-card pl-2 pr-2'>
-              <li className='item'>
-                <figure className='product-img m-1'>
+            <ul className='products-list product-list-in-card'>
+              <li className='item d-flex'>
+                <figure className='col-4 col-md-2'>
                   <img
                     src='https://via.placeholder.com/100'
                     alt='Placeholder Business Profile'
                     className='image-fluid'
                   />
-                  <p className='bus-details--rating'>
-                    <img src='star.svg' alt='Star Icon' />
-                    <img src='star.svg' alt='Star Icon' />
-                    <img src='star.svg' alt='Star Icon' />
-                    <img src='star.svg' alt='Star Icon' />
+                  <p className='bus-details--rating '>
+                    <img
+                      src='star.svg'
+                      alt='Star Icon'
+                      className='pr-1 pl-1 col-3 col-md-3'
+                    />
+                    <img
+                      src='star.svg'
+                      alt='Star Icon'
+                      className='pr-1 pl-1 col-3 col-md-3'
+                    />
+                    <img
+                      src='star.svg'
+                      alt='Star Icon'
+                      className='pr-1 pl-1 col-3 col-md-3'
+                    />
+                    <img
+                      src='star.svg'
+                      alt='Star Icon'
+                      className='pr-1 pl-1 col-3 col-md-3'
+                    />
                   </p>
                   <button className='btn btn-default'>Review</button>
                 </figure>
-                <div className='product-info'>
+                <div className='product-info col-7 ml-4'>
                   <h1 className='product title'>Business Name</h1>
-                  <span className='float-right'>
+                  <span className='float-right col-md-auto me-md-auto'>
                     12 Main Street Toronto, ON L3K 5H7
                   </span>
-                  <span className='product-description'>
+                  <span className='product-description col-md-auto me-md-auto'>
                     The business description goes here.
                   </span>
                 </div>
