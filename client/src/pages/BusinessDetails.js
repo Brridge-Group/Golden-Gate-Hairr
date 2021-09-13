6224548030597334e391c309acb6e0d10ca8fa49
Kathy <Kathy@KathyLambert.me>
2021-09-13 19:56:01 -0400

    Fix (BusDetails): Modify structure to map through DB businesses

--- client/src/pages/BusinessDetails.js
+++ client/src/pages/BusinessDetails.js
@@ -33,58 +33,67 @@ const BusinessDetails = () => {
         <div className='card'>
           <div className='card-body'>
             <ul className='products-list product-list-in-card'>
-              <li className='item d-flex'>
-                <figure className='col-4 col-md-2'>
-                  <img
-                    src='https://via.placeholder.com/100'
-                    alt='Placeholder Business Profile'
-                    className='image-fluid'
-                  />
-                  <p className='bus-details--rating '>
-                    <img
-                      src='star.svg'
-                      alt='Star Icon'
-                      className='pr-1 pl-1 col-3 col-md-3 col-xl-2'
-                    />
-                    <img
-                      src='star.svg'
-                      alt='Star Icon'
-                      className='pr-1 pl-1 col-3 col-md-3 col-xl-2'
-                    />
-                    <img
-                      src='star.svg'
-                      alt='Star Icon'
-                      className='pr-1 pl-1 col-3 col-md-3 col-xl-2'
-                    />
-                    <img
-                      src='star.svg'
-                      alt='Star Icon'
-                      className='pr-1 pl-1 col-3 col-md-3 col-xl-2'
-                    />
-                  </p>
-                  <button className='btn btn-default'>Review</button>
-                </figure>
-                <div className='product-info col-7 ml-4 col-md-9'>
-                  {fetchedBusinessUsers.map(business => {
-                    return (
-                      <>
-                        <h1 className='product title'>{business.name}</h1>
-                        <span className='float-right col-md-auto me-md-auto'>
-                          {business.address1}, {business.address2}
-                          {business.city}, {business.state} {business.zipCode}
-                        </span>
-                        <span className='product-description col-md-auto me-md-auto'>
-                          {business.description}
-                        </span>
-                      </>
-                    )
-                  })}
-                </div>
-              </li>
+              {fetchedBusinessUsers.map((business) => {
+                return (
+                  <li className='item d-flex'>
+                    <figure className='col-4 col-md-2'>
+                      <img
+                        src='https://via.placeholder.com/100'
+                        alt='Placeholder Business Profile'
+                        className='image-fluid'
+                      />
+                      <p className='bus-details--rating '>
+                        <img
+                          src='star.svg'
+                          alt='Star Icon'
+                          className='pr-1 pl-1 col-3 col-md-3 col-xl-2'
+                        />
+                        <img
+                          src='star.svg'
+                          alt='Star Icon'
+                          className='pr-1 pl-1 col-3 col-md-3 col-xl-2'
+                        />
+                        <img
+                          src='star.svg'
+                          alt='Star Icon'
+                          className='pr-1 pl-1 col-3 col-md-3 col-xl-2'
+                        />
+                        <img
+                          src='star.svg'
+                          alt='Star Icon'
+                          className='pr-1 pl-1 col-3 col-md-3 col-xl-2'
+                        />
+                      </p>
+                      <button className='btn btn-default'>Review</button>
+                    </figure>
+                    <div className='product-info col-7 ml-4 col-md-9'>
+                      <h1
+                        className='product title'
+                        style={{ fontSize: '1.75rem' }}
+                      >
+                        {business.businessName}
+                      </h1>
+                      <span className='float-right col-md-auto me-md-auto'>
+                        {!business.address1 ? (
+                          <div></div>
+                        ) : (
+                          <div>
+                            {business.address1}, {business.address2}
+                            {business.city}, {business.state} {business.zipCode}
+                          </div>
+                        )}
+                      </span>
+                      <span className='product-description col-md-auto me-md-auto'>
+                        {business.description}
+                      </span>
+                      <div className='text-center'>
+                        <button className='btn btn-default'>Book Now</button>
+                      </div>
+                    </div>
+                  </li>
+                )
+              })}
             </ul>
-            <div className='text-center'>
-              <button className='btn btn-default'>Book Now</button>
-            </div>
           </div>
         </div>
       </section>
