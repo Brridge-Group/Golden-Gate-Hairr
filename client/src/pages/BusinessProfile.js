3bfb30ea8e10ba17ec826ee387bb52cd1f7f5e1f
Kathy <Kathy@KathyLambert.me>
2021-09-18 22:39:56 -0400

    Feat: Add <BusinessProfile/>

---
+++ client/src/pages/BusinessProfile.js
@@ -0,0 +1,15 @@
+// Custom Imports
+import ContentHeader from '../components/ContentHeader'
+
+const BusinessProfile = () => {
+  return (
+    <>
+      <section className='content-wrapper bus-Profile'>
+        <header className='bus-Profile--header'>
+          <ContentHeader title='Business Profile Page' />
+        </header>
+      </section>
+    </>
+  )
+}
+export default BusinessProfile
