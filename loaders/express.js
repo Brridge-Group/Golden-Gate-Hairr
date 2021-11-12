6c3b7263841269587b0f7b354d7bee3013831911
Kathy <Kathy@KathyLambert.me>
2021-11-12 13:20:54 -0500

    Feat: Add features and services routes

--- loaders/express.js
+++ loaders/express.js
@@ -3,19 +3,24 @@ const path = require('path')
 const generatePassword = require('password-generator')
 const cors = require('cors')
 
+// Require Routes
 const itemsRoutes = require('../routes/items-route')
-const businessRoutes = require('../routes/business-route')
+const businessesRoutes = require('../routes/businesses-route')
 const usersRoutes = require('../routes/users-route')
+const featuresRoutes = require('../routes/features-route')
+const servicesRoutes = require('../routes/services-route')
 
-const loader = async (app) => {
+const loader = async app => {
   app.use(express.json())
 
   // Serve static files from the React app
   app.use(express.static(path.join(__dirname, '../client/build')))
 
   app.use('/api/items', itemsRoutes)
-  app.use('/api/businesses', businessRoutes)
+  app.use('/api/businesses', businessesRoutes)
   app.use('/api/users', usersRoutes)
+  app.use('/api/features', featuresRoutes)
+  app.use('/api/services', servicesRoutes)
 
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
