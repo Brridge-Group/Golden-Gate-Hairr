5a8f1fa9911d150441bdd772f109ec80f6c57faa
Kathy <Kathy@KathyLambert.me>
2021-11-12 13:08:37 -0500

    Feat: Add services model

---
+++ models/service.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const serviceSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+})
+
+module.exports = mongoose.model('Service', serviceSchema)
