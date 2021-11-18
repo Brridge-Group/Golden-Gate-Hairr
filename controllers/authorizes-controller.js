3151fb521396ea1d23c610c84bb49f5f361d0276
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-11-18 12:43:49 -0500

    trashing this branch too complicated

--- controllers/authorizes-controller.js
+++ controllers/authorizes-controller.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+// const User = require('../models/user')
+const db = require('../models')
+const User = db.user
+const Role = db.role
 
 const signup = async (req, res) => {
   try {
@@ -28,6 +31,50 @@ const signup = async (req, res) => {
       password: encryptedPassword,
       isOwner,
     })
+
+    if (req.body.roles) {
+      Role.find(
+        {
+          name: { $in: req.body.roles }
+        },
+        (err, roles) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+
+          user.roles = roles.map(role => role._id);
+          user.save(err => {
+            if (err) {
+              res.status(500).send({ message: err });
+              return;
+            }
+
+            res.send({ message: "User was registered successfully!" });
+          });
+        }
+      );
+    } else {
+      Role.findOne({ name: "user" }, (err, role) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        user.roles = [role._id];
+        user.save(err => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+
+          res.send({ message: "User was registered successfully!" });
+        });
+      });
+    }
+  });
+};
+
 
     const token = jwt.sign(user.toObject(), process.env.TOKEN_KEY)
     return res.status(201).send({ success: true, user: user.toObject(), token })
