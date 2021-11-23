7af8af51a0b1b09df5171434d76ca7fa5b208091
CodeMeKathy * <Kathy@KathyLambert.me>
2021-11-23 11:16:11 -0500

    Feat: Update services model

--- models/service.js
+++ models/service.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const serviceSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  isChecked: { type: Boolean, default: false },
 })
 
 module.exports = mongoose.model('Service', serviceSchema)
