3667246dd1de288d3675ba75461a06f148f4a45a
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-11-28 16:42:18 -0500

    added form to review, redirect to home on submit

--- client/src/pages/Review.js
+++ client/src/pages/Review.js
@@ -2,29 +2,39 @@ import React, { useState } from 'react'
 import ContentHeader from '../components/ContentHeader'
 import { useHistory } from 'react-router-dom'
 
-
-
 const Review = () => {
   const history = useHistory();
   const business = history.location.state.business;
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [reviewForm, setReviewForm] = useState({
+    comment: ''
+  })
 
   console.log(business)
+  const { comment } = reviewForm
 
   const submitReview = () => {
 
+  }
+
+  const routeChange = () => {
+    history.push('/')
+  }
+
+  const handleChange = e => {
+    setReviewForm({ ...reviewForm, [e.target.name]: e.target.value })
   }
 
   return (
     <React.Fragment>
       <ContentHeader title='Search' />
       <div className='card w-50 mx-auto'>
-      <div className='card-body' >
-    <div className='review-container'>
+      <div className='card-body review' >
+    {/* <div className='review-container'> */}
             <h6 className='m-0'>Review your experience with {business.businessName} </h6>
             <form className='form' onSubmit={submitReview}>
-    <div className="star-rating">
+    <div className="form-group star-rating">
       {[...Array(5)].map((star, index) => {
         index += 1;
         return (
@@ -41,11 +51,30 @@ const Review = () => {
         );
       })}
             </div>
+            <div className='form-group'>
+              <label htmlFor='comment'></label>
+              <textarea
+                name='comment'
+                type='text'
+                className='form-control'
+                onChange={handleChange}
+                value={comment}
+              />
+            </div>
+            <button
+                onChange={handleChange}
+                type='submit'
+              className='btn btn-primary'
+              onClick={routeChange}
+            >
+
+                Submit
+              </button>
 
             </form>
       </div>
       </div>
-      </div>
+      {/* </div> */}
       </React.Fragment>
   );
 }
