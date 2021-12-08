ef671a5285c951ed8bb680c3d40efc225cd8ffcb
CodeMeKathy * <Kathy@KathyLambert.me>
2021-12-08 02:45:19 -0500

    Feat (BizDetails/>): Add feats and services data from context

--- client/src/pages/BusinessDetails.js
+++ client/src/pages/BusinessDetails.js
@@ -1,11 +1,16 @@
+// React Components
 import { useEffect, useState } from 'react'
+
+// Custom Imports
 import { withContext } from '../contexts/AppContext'
 import ContentHeader from '../components/ContentHeader'
+import LoadSpinner from '../components/LoadSpinner'
 import '../stylesheets/Businesses.css'
 
 const BusinessDetails = props => {
-  const [business, setBusiness] = useState('')
+  const [business, setBusiness] = useState(props.history.location.business)
   const [phone, setPhone] = useState(false)
+  console.log('business', business)
 
   useEffect(() => {
     setBusiness(props.history.location.business)
@@ -15,164 +20,254 @@ const BusinessDetails = props => {
     setPhone(true)
   }
 
+  // Initialize state for features and services whole data obj from context
+  const [features, setFeatures] = useState([])
+  const [services, setServices] = useState([])
+
+  useEffect(() => {
+    setFeatures(props.feats)
+    setServices(props.services)
+  }, [props.features, props.services])
+
+  // console.log('features', features)
+  // console.log('services', services)
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setIsLoading(true)
+    const fetchFeatsServices = async () => {
+      await props.fetchFeatures()
+      await props.fetchServices()
+    }
+    fetchFeatsServices()
+    return function clean() {
+      setIsLoading(false)
+    }
+  }, [])
+
+  // Initialize state for features and services  array from context
+  const [bizFeatsArr, setBizFeatsArr] = useState([])
+  const [bizServiceArr, setBizServiceArr] = useState([])
+
+  useEffect(() => {
+    setIsLoading(true)
+    const fetchDetails = async () => {
+      const bizFeats = await props.feats
+      const bizServices = await props.services
+      let businessCopy = await props.history.location.business
+      let bizFeatsArr = []
+      let bizServiceArr = []
+      // for (let i = 0; i < businessCopy?.features.length; i++) {
+      //   console.log('business.features[i]', businessCopy.features[i])
+      //   let bizFeats = businessCopy.features.map(feat => {
+      //     features.features.find(feat => {
+      //       if (feat.id === businessCopy.features[i]) {
+      //         return feat
+      //       }
+      //     })
+      //   })
+      // }
+
+      businessCopy?.features.map(bizFeat => {
+        // console.log('bizFeat', bizFeat)
+        bizFeats?.features.find(feat => {
+          if (feat.id === bizFeat) {
+            bizFeatsArr.push(feat)
+          }
+        })
+        setBizFeatsArr(bizFeatsArr)
+      })
+      businessCopy?.services.map(bizService => {
+        // console.log(bizService)
+        bizServices?.services.find(service => {
+          if (service.id === bizService) {
+            bizServiceArr.push(service)
+          }
+        })
+        setBizServiceArr(bizServiceArr)
+      })
+    }
+    // let bizFeats = business.features.map(feat => {
+    //   feat.find(feat => {
+    //     if (feat.id === business.features[i]) {
+    //       return feat
+    //     }
+    //   })
+    // })
+    // $reactTemp0.find(feat => {
+    //   if ('611d140afede12d4e2db1fcd' === feat._id) console.log(feat._id)
+    //   return true
+    // })
+    // console.log('business', business)
+    // }
+    fetchDetails()
+    return function clean() {
+      setIsLoading(false)
+    }
+  }, [business])
+
+  useEffect(() => {
+    setIsLoading(true)
+    setBizFeatsArr(bizFeatsArr)
+    setBizServiceArr(bizServiceArr)
+    return function clean() {
+      setIsLoading(false)
+    }
+  }, [bizServiceArr, bizFeatsArr])
+
+  console.log('bizServiceArr', bizServiceArr)
+  console.log('arr', bizFeatsArr)
+
   return (
     <>
-      {/*(Backlog) TODO: [ ] - ? Remove all ContentHeaders or modify and utilize across app  */}
-      {/* <ContentHeader title='Business Details ' /> */}
-      <section className='content'>
-        <div className='card mx-auto'>
-          <div className='card-body'>
-            <div className='products-list product-list-in-card d-flex justify-content-around align-content-center'>
-              <div className='md-10'>
-                <figure className='bus-details--image'>
-                  <div className='image--container'>
-                    <img
-                      src='https://via.placeholder.com/100'
-                      alt='Placeholder Business Profile Image'
-                      className='image-fluid'
-                    />
-                    <p className='bus-details--rating'>
-                      <img
-                        src='star.svg'
-                        alt='Star Icon'
-                        style={{ width: '10%' }}
-                      />
-                      <img
-                        src='star.svg'
-                        alt='Star Icon'
-                        style={{ width: '10%' }}
-                      />
-                      <img
-                        src='star.svg'
-                        alt='Star Icon'
-                        style={{ width: '10%' }}
-                      />
-                      <img
-                        src='star.svg'
-                        alt='Star Icon'
-                        style={{ width: '10%' }}
-                      />
-                    </p>
+      {!props.loading ? (
+        <>
+          {/*(Backlog) TODO: [ ] - ? Remove all ContentHeaders or modify and utilize across app  */}
+          {/* <ContentHeader title='Business Details ' /> */}
+          <section className='content'>
+            <div className='card mx-auto'>
+              <div className='card-body'>
+                <div className='products-list product-list-in-card d-flex justify-content-around align-content-center'>
+                  <div className='m-3'>
+                    <figure className='bus-details--image'>
+                      <div className='image--container'>
+                        <img
+                          src='https://via.placeholder.com/100'
+                          alt='Placeholder Business Profile Image'
+                          className='image-fluid'
+                        />
+                        <p className='bus-details--rating'>
+                          <img
+                            src='star.svg'
+                            alt='Star Icon'
+                            style={{ width: '10%' }}
+                          />
+                          <img
+                            src='star.svg'
+                            alt='Star Icon'
+                            style={{ width: '10%' }}
+                          />
+                          <img
+                            src='star.svg'
+                            alt='Star Icon'
+                            style={{ width: '10%' }}
+                          />
+                          <img
+                            src='star.svg'
+                            alt='Star Icon'
+                            style={{ width: '10%' }}
+                          />
+                        </p>
+                      </div>
+                    </figure>
+                    <div className='mx-auto d-flex flex-column'>
+                      {props.token ? (
+                        <button className='btn btn-default'>Review</button>
+                      ) : (
+                        ''
+                      )}
+                    </div>
                   </div>
-                </figure>
-                <div className='mx-auto d-flex flex-column'>
-                  {props.token ? (
-                    <button className='btn btn-default'>Review</button>
-                  ) : (
-                    ''
-                  )}
-                </div>
-              </div>
-              <div className='bus-details d-flex flex-column'>
-                <div className='d-flex justify-content-between'>
-                  <div className='bus-details--header'>
-                    <h1 className='card-header border-bottom-0'>
-                      {business.businessName}
-                    </h1>
-                    <div className='product-description'>
-                      {business.description}
+                  <div className='bus-details d-flex flex-column'>
+                    <div className='d-flex justify-content-between'>
+                      <div className='bus-details--header'>
+                        <h1 className='card-header border-bottom-0'>
+                          {business.businessName}
+                        </h1>
+                        <div className='product-description'>
+                          {business.description}
+                        </div>
+
+                        <br />
+                      </div>
+                      <div className='bus-details--info mt-3'>
+                        <div className='bus-details--address small'>
+                          <b>Address:</b>
+                          <br />
+                          {business.address1} {business.city}, {business.state}{' '}
+                          {business.zipCode}
+                        </div>
+                        <div className='bus-details--email small'>
+                          <b>Email:</b>
+                          <br />
+                          {business.email}
+                        </div>
+                      </div>
                     </div>
 
-                    <br />
-                  </div>
-                  <div className='bus-details--info mt-3'>
-                    <div className='bus-details--address small'>
-                      <b>Address:</b>
-                      <br />
-                      {business.address1} {business.city}, {business.state}{' '}
-                      {business.zipCode}
+                    <div className='d-flex justify-content-around'>
+                      <div className='bus-details--features-list w-50'>
+                        <p className='card-header border-bottom-0'>
+                          <b>Features</b>
+                        </p>
+                        {bizFeatsArr.map(feat => (
+                          <div
+                            className='bus-details--features'
+                            style={{ textTransform: 'capitalize' }}
+                          >
+                            <ul>
+                              <li>
+                                <b>{feat.name}</b>
+                                <br />
+                                <span className='small'>
+                                  {feat.description}
+                                </span>
+                              </li>
+                            </ul>
+                          </div>
+                        ))}
+                      </div>
+                      <div className='bus-details--services-list w-50'>
+                        <p className='card-header border-bottom-0'>
+                          <b>Services</b>
+                        </p>
+                        {bizServiceArr.map(service => (
+                          <div
+                            className='bus-details--service-name'
+                            style={{ textTransform: 'capitalize' }}
+                          >
+                            <ul>
+                              <li>
+                                <b>{service.name}</b>
+                                <br />
+                                <span
+                                  className='small'
+                                  style={{ textTransform: '' }}
+                                >
+                                  {service.description}
+                                </span>
+                              </li>
+                            </ul>
+                          </div>
+                        ))}
+                      </div>
                     </div>
-                    <div className='bus-details--email small'>
-                      <b>Email:</b>
-                      <br />
-                      {business.email}
+                    <div className='d-flex flex-column'>
+                      <div className={phone ? 'visable-phone' : 'hidden-phone'}>
+                        phone: {business.phone}
+                      </div>
+                      <button
+                        onClick={handleClick}
+                        className='btn btn-default'
+                        style={{
+                          width: '75%',
+                          marginTop: '10px',
+                          margin: 'auto',
+                        }}
+                      >
+                        Book Now
+                      </button>
                     </div>
                   </div>
-                </div>
-                <div className='d-flex flex-column'>
-                  <div className={phone ? 'visable-phone' : 'hidden-phone'}>
-                    phone: {business.phone}
-                  </div>
-                  <button
-                    onClick={handleClick}
-                    className='btn btn-default'
-                    style={{ width: '75%', marginTop: '10px', margin: 'auto' }}
-                  >
-                    Book Now
-                  </button>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-
-      {/* <div className='card w-50 mx-auto'>
-        <div className='card-body'>
-          <ul className='products-list product-list-in-card'>
-            <li className='item'>
-              <figure className='indie-card'>
-                <div className='pic-star-container'>
-                  <img
-                    src='https://via.placeholder.com/100'
-                    alt='Placeholder Business Profile'
-                    className='image-fluid'
-                  />
-                  <p className='bus-details--rating '>
-                    <img
-                      src='star.svg'
-                      alt='Star Icon'
-                      style={{ width: '10%' }}
-                    />
-                    <img
-                      src='star.svg'
-                      alt='Star Icon'
-                      style={{ width: '10%' }}
-                    />
-                    <img
-                      src='star.svg'
-                      alt='Star Icon'
-                      style={{ width: '10%' }}
-                    />
-                    <img
-                      src='star.svg'
-                      alt='Star Icon'
-                      style={{ width: '10%' }}
-                    />
-                  </p>
-                  {props.token ? (
-                    <button className='btn btn-default'>Review</button>
-                  ) : (
-                    ''
-                  )}
-                </div>
-                <div>
-                  <h1 className='product title'>{business.businessName}</h1>
-                  <div>
-                    {business.address1} {business.city}, {business.state}{' '}
-                    {business.zipCode}
-                    <br />
-                    email: {business.email}
-                    <div className={phone ? 'visable-phone' : 'hidden-phone'}>
-                      phone: {business.phone}
-                    </div>
-                  </div>
-                  <div className='product-description'>
-                    {business.description}
-                  </div>
-                  <button
-                    onClick={handleClick}
-                    className='btn btn-default'
-                    style={{ marginTop: '10px' }}
-                  >
-                    Book Now
-                  </button>
-                </div>
-              </figure>
-            </li>
-          </ul>
-        </div>
-      </div> */}
+          </section>
+        </>
+      ) : (
+        isLoading && <LoadSpinner />
+      )}
     </>
   )
 }
