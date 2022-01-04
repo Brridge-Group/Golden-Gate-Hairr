a69289b3b9252cd852e5d448a755c5ded8541e6e
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-01-03 20:07:17 -0500

    mapped reviews in profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,17 +4,16 @@ import axios from 'axios'
 
 const Profile = props => {
   const [userReviews, setUserReviews] = useState([])
+  const userReviewArr = []
 
   const fetchUserReviews = async () => {
     try {
-      const response = await fetch(`/api/reviews/`, { method: 'GET' })
+      const response = await fetch(`/api/reviews`)
       const responseData = await response.json()
 
       if (!response.ok) {
         throw new Error(responseData.message)
       }
-      const userReviewArr = []
-      console.log('fetched responseData.reviews', responseData.reviews)
       const reviews = responseData.reviews
       reviews.map(review => {
         props.user.reviews.find(userReview => {
@@ -22,15 +21,12 @@ const Profile = props => {
             userReviewArr.push(review)
           }
         })
-        console.log(userReviewArr)
-        return setUserReviews(userReviewArr)
       })
     } catch (err) {
       console.log(err)
     }
-    console.log(userReviews)
+    setUserReviews(userReviewArr)
   }
-  console.log(userReviews)
 
   return (
     <div className='card w-50 mx-auto'>
@@ -38,20 +34,17 @@ const Profile = props => {
         Hi {props.user.firstName.slice(0, 1).toUpperCase() + props.user.firstName.slice(1).toLowerCase()}!
         <br />
         Here are your reviews.
-        {props.user.reviews}
-        {userReviews.map(review, index => {
+        {console.log('userReviews in return', userReviews)}
+        {userReviews.map((userRev, index) => {
           return (
             <ul>
               <li key={index}>
-                {review.comment}
-                {review.rating}
+                {userRev.comment}
+                {userRev.rating}
               </li>
             </ul>
           )
         })}
-        {/* {props.user.reviews.map(review => {
-          return { review }
-        })} */}
         <button onClick={fetchUserReviews} className='btn btn-default'></button>
       </div>
     </div>
