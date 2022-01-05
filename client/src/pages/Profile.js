d8a9b3e5e9410020eec294cfac57e35a83a8ed9f
CodeMeKathy * <Kathy@KathyLambert.me>
2022-01-05 13:43:33 -0500

    Feat: Add initial user and business owner profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,120 @@
+// Custom Imports
 import { withContext } from '../contexts/AppContext'
-
+import '../stylesheets/Profile.css'
 const Profile = props => {
   // const { currentUser } = props
   console.log('in profile user', props, props.user)
 
-  return <div>hi {props.user.firstName}!</div>
+  return (
+    <>
+      <section className='profile'>
+        <div className='profile-header'>
+          <h1>{props.user.role} Profile</h1>
+          {/* <h2>hi {props.user.firstName}!</h2> */}
+        </div>
+        <div className='row'>
+          <div className='col-md-9'>
+            <div className='card-body'>
+              <div className='tab-content'>
+                <div className='tab-pane active' id='Profile Details'>
+                  <form className='form-horizontal'>
+                    <div className='form-group row'>
+                      <label htmlFor='inputName' className='col-sm-2 col-form-label'>
+                        Name
+                      </label>
+                      <div className='col-sm-10'>
+                        <input type='email' className='form-control' id='inputName' defaultValue={props.user.firstName} />
+                      </div>
+                    </div>
+                    <div className='form-group row'>
+                      <label htmlFor='inputName2' className='col-sm-2 col-form-label'>
+                        Name
+                      </label>
+                      <div className='col-sm-10'>
+                        <input type='text' className='form-control' id='inputName2' defaultValue={props.user.lastName} />
+                      </div>
+                    </div>
+                    <div className='form-group row'>
+                      <label htmlFor='inputEmail' className='col-sm-2 col-form-label'>
+                        Email
+                      </label>
+                      <div className='col-sm-10'>
+                        <input type='email' className='form-control' id='inputEmail' defaultValue={props.user.email} />
+                        {/* <input type='email' className='form-control' id='inputEmail' placeholder={props.user.email} /> */}
+                      </div>
+                    </div>
+                    <div className='form-group row'>
+                      <label htmlFor='inputExperience' className='col-sm-2 col-form-label'>
+                        Reviews
+                      </label>
+                      <div className='col-sm-10'>
+                        <textarea className='form-control' id='inputExperience' defaultValue={props.user.reviews}></textarea>
+                      </div>
+                    </div>
+                    {/* <div className='form-group row'>
+                      <label htmlFor='inputSkills' className='col-sm-2 col-form-label'>
+                        Skills
+                      </label>
+                      <div className='col-sm-10'>
+                        <input type='text' className='form-control' id='inputSkills' placeholder='Skills' />
+                      </div>
+                    </div> */}
+                    <div className='form-group row'>
+                      <div className='offset-sm-2 col-sm-10'>
+                        <div className='checkbox'>
+                          <label>
+                            <input type='checkbox' defaultChecked={props.user.isOwner} /> Business Owner
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='form-group row'>
+                      <div className='offset-sm-2 col-sm-10'>
+                        <button type='submit' className='btn btn-danger'>
+                          Submit
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-3'>
+            <div className='card card-primary' id='card-outline'>
+              <div className='card-body box-profile'>
+                <div className='text-center'>
+                  <img className='profile-user-img img-fluid img-circle' src='/assets/dist/img/user2-160x160.jpg' alt='User profile picture' />
+                </div>
+
+                <h3 className='profile-username text-center'>
+                  {props.user.firstName} {props.user.lastName}
+                </h3>
+
+                <p className='text-muted text-center'>{props.user.role}</p>
+
+                <ul className='list-group list-group-unbordered mb-3'>
+                  <li className='list-group-item'>
+                    <b>Reviews</b> <a className='float-right'>{props.user.reviews.length}</a>
+                  </li>
+                  {/* <li className='list-group-item'>
+                    <b>Following</b> <a className='float-right'>543</a>
+                  </li>
+                  <li className='list-group-item'>
+                    <b>Friends</b> <a className='float-right'>13,287</a>
+                  </li> */}
+                </ul>
+
+                <a href='#' className='btn btn-primary btn-block'>
+                  <b>Follow</b>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default withContext(Profile)
